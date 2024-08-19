7b88441571caa258027437fd5709b4e7f8404dc7
Dinesh_Gosavi <dinesh@damacloid.com>
2024-08-19 17:27:31 +0530

    connected to the database

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const express = require('express');
+const { default: mongoose } = require('mongoose');
+const connectDb = require('../db/connect');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+const connect = connectDb(process.env.MongoDbUri);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ db/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDb = async (url) => {
+  try {
+    const connect = await mongoose.connect(url);
+    console.log(
+      "Database connected: ",
+      connect.connection.host,
+      connect.connection.name
+    );
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDb;
