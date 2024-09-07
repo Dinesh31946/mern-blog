e5b0efdf91cd0b5db0ad927ac6a78040a3df3491
Dinesh_Gosavi <dinesh@damacloid.com>
2024-09-07 16:19:21 +0530

    server.js file changes for deployment

--- api/server.js
+++ api/server.js
@@ -28,10 +28,10 @@ app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
 app.use("/api/comment", commentRoute);
 
-app(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 // error handling middleware
